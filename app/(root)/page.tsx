8fd1acfcd39da6bad857bd81725e2163223ed829
Nathan2128 <nathan.sinnat@gmail.com>
2025-04-18 23:32:02 -0400

    working interview session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import InterviewCard from "../components/InterviewCard";
-import { get } from "http";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import {
-  getCurrentUser,
   getInterviewsByUserId,
   getLatestInterviews,
-} from "@/lib/actions/auth.action";
+} from "@/lib/actions/general.action";
 
 const Page = async () => {
   const user = await getCurrentUser();
--- app/components/InterviewCard.tsx
+++ app/components/InterviewCard.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import DisplayTechIcons from "./DisplayTechIcons";
 
 const InterviewCard = ({
-  interviewId,
+  id,
   userId,
   role,
   type,
@@ -58,11 +58,7 @@ const InterviewCard = ({
           <DisplayTechIcons techstack={techstack} />
           <Button className="btn-primary">
             <Link
-              href={
-                feedback
-                  ? `/interview/${interviewId}/feedback`
-                  : `/interview/${interviewId}`
-              }
+              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
             >
               {feedback ? "Check Feedback" : "Start Interview"}
             </Link>
